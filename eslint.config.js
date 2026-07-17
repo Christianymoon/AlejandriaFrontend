9b37733fe52070fc967be8f0b0a2f19c51b10093
Christian <chrisvergara7@outlook.com>
2026-07-16 18:28:56 -0600

    fix eslint version 1

--- eslint.config.js
+++ eslint.config.js
@@ -2,20 +2,18 @@ import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
-import { reactRefresh } from "eslint-plugin-react-refresh";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], 
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
     plugins: {
       js,
-    },
-    plugins: {
-      "react-refresh": reactRefresh.plugin,
     },
     rules: {
       'react-refresh/only-export-components': 'warn',
     },
-    extends: ["js/recommended"], 
-    languageOptions: { globals: globals.browser } },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser }
+  },
   pluginReact.configs.flat.recommended,
 ]);
